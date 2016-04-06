d39fd25f92e95605494dc21392ceae5e75dc8a06
Nate Foreman <nforeman@pillartechnology.com>
2016-04-06 19:30:56 -0400

    First Commit

---
+++ src/test/java/com/katas/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.katas;
+
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class StringCalculatorTest {
+
+}
